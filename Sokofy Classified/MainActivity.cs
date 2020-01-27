1ae0eca47a5243a056a890c33fd2d9afec78c877
Dennis Aziaya <aziayadennis@gmail.com>
2020-01-27 23:44:21 +0300

    Add project files.

---
+++ Sokofy Classified/MainActivity.cs
@@ -0,0 +1,53 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Support.V7.App;
+using Android.Runtime;
+using Android.Widget;
+using Android.Webkit;
+using Android.Views;
+
+namespace Sokofy_Classified
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        private WebView _webView;
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.activity_main);
+
+            _webView = FindViewById<WebView>(Resource.Id.webview);
+            _webView.Settings.JavaScriptEnabled = true;
+            _webView.SetWebViewClient(new HelloWebViewClient());
+            _webView.LoadUrl("https://www.sokofy.co.ke");
+        }
+
+
+        public class HelloWebViewClient : WebViewClient
+        {
+            public override bool ShouldOverrideUrlLoading(WebView view, string url)
+            {
+                view.LoadUrl(url);
+                return false;
+            }
+
+        }
+
+        public override bool OnKeyDown(Android.Views.Keycode keyCode, Android.Views.KeyEvent e)
+        {
+            if (keyCode == Keycode.Back && _webView.CanGoBack())
+            {
+                _webView.GoBack();
+                return true;
+            }
+            return base.OnKeyDown(keyCode, e);
+        }
+
+
+    }
+
+
+}
